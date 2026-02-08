9748abf299997e179da3568a7de43b0cfb3efb7c
LAULAUtonton <lestevezplasencia@gmail.com>
2026-02-08 21:01:25 +0000

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -9,6 +9,13 @@ from pydantic import BaseModel, Field, ConfigDict
 from typing import List, Optional
 import uuid
 from datetime import datetime, timezone
+# from motor.motor_asyncio import AsyncIOMotorClient
+# import os
+
+# MONGO_URL = os.environ["MONGO_URL"]
+# client = AsyncIOMotorClient(MONGO_URL)
+# db = client["globalissues"]
+
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
